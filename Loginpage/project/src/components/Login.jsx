e472f0a53a81e17a4abeb9e8fdb546293c0109b5
krishhackz <krishhackz.in@gmail.com>
2024-12-17 23:07:10 +0530

    Creating login form in react

---
+++ Loginpage/project/src/components/Login.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Login(){
+    return <>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+          />
+        </div>
+
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+          />
+        </div>
+
+        <button type="submit">Login</button>
+    </>
+}
