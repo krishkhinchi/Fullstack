6d5b7705321a07dc5f145231f9d9ab71cff84d68
krishhackz <krishhackz.in@gmail.com>
2024-12-30 10:13:55 +0530

    server

--- Backend/Connection/Server.js
+++ Backend/Connection/Server.js
@@ -1,7 +1,20 @@
-const express=require('express')
-const app=express();
-const connectDb=require('./config/db')
-connectDb()
-app.listen(4000,()=>{
-    console.log("Server is running... ")
-})
+const express = require("express");
+const app = express();
+const connectDb = require("./config/db");
+const userModel = require("./model/userSchema");
+
+connectDb();
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.send("HEllo");
+});
+app.post("/register",async (req, res) => {
+  const {email,password,name} = req.body;
+  const userExist=await userModel.findOne({email})
+  console.log(userExist)
+});
+app.listen(4000, () => {
+  console.log("Server is running... ");
+});
