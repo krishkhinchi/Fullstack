f246b82f264c32e680f1c48f878e93e558c3f52c
krishhackz <krishhackz.in@gmail.com>
2024-12-30 16:40:34 +0530

    Check email is exist or not

--- Backend/Connection/Server.js
+++ Backend/Connection/Server.js
@@ -8,12 +8,17 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-  res.send("HEllo");
+  res.send("HEllO");
 });
-app.post("/register",async (req, res) => {
-  const {email,password,name} = req.body;
-  const userExist=await userModel.findOne({email})
-  console.log(userExist)
+app.post("/register", async (req, res) => {
+  const { email, password, name } = req.body;
+  const userExist = await userModel.findOne({ email });
+  if (userExist) {
+    res.send({ message: "User Exist" });
+  }
+  const newUser = new userModel({ name, email, password });
+  await newUser.save();
+  res.send({ message: "User Created Successfully" });
 });
 app.listen(4000, () => {
   console.log("Server is running... ");
