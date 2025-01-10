79ff75f7185dd4a0b01379e950b7dee2db0839be
krishhackz <krishhackz.in@gmail.com>
2025-01-10 10:18:44 +0530

    Import Filter.jsx

--- Frontend/project/src/App.js
+++ Frontend/project/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Form from "./components/Form";
+import Filter from "./components/Filter";
 
 export default function App() {
   return <>
-    <Form/>      
+    <Filter/>      
   </>
 }
