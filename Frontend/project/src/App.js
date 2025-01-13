b130bef5f4b0d4feb9dd510abfec482cd1bac7df
krishhackz <krishhackz.in@gmail.com>
2025-01-13 12:51:34 +0530

    Calling Todo.jsx

--- Frontend/project/src/App.js
+++ Frontend/project/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import Form from "./components/Form";
-import Filter from "./components/Filter";
+import Todo from "./components/Todo";
 
 export default function App() {
   return <>
-    <Filter/>      
+    <Todo/>      
   </>
 }
