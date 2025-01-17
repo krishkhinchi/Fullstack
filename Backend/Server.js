2a7b1a717da1aac99fb8139139394e3cf9be97ef
krishhackz <krishhackz.in@gmail.com>
2025-01-17 15:47:22 +0530

    import and use 'Cors' in Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-var cors = require('cors')
+const cors = require('cors')
 
 const connectDb = require("./config/db");
 const userModel = require("./model/userSchema");
