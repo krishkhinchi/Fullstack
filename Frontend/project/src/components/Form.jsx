8c354c60b96c04573a564deaaa7ef0875f45e2ad
krishhackz <krishhackz.in@gmail.com>
2025-01-17 15:32:12 +0530

    Frontend and Backend Connection

--- Frontend/project/src/components/Form.jsx
+++ Frontend/project/src/components/Form.jsx
@@ -1,43 +1,71 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const Form = () => {
+export default function Form(){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
 
-const [email,setEmail]=useState('');
-const [password,setPassword]=useState('');
-const handleSubmit=(e)=>{
-e.preventDefault();
-const data=email;
-const data1=password;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      password,
+      email,
+      name,
+    };
+    try {
+      const response = await axios.post("http://localhost:4000/register", data);
+      console.log(response.status);
+      if (response.status === 200) {
+        navigate("/todo");
+      } else {
+        navigate("/Form");
+      }
+    } catch (err) {}
+  };
 
-try{
-    const response=backendAPI('localhost4000/register',post,data,data1);
-    if(response ==200){
-    }
-}
-
-}
   return (
     <div>
-<form   onSubmit={handleSubmit} >
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" 
-    value={email}
-    onChange={(e)=>setEmail(e.target.value)}
-    placeholder="Enter email"/>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" 
-    value={password}
-    onChange={(e)=>setPassword(e.target.value)}
-    placeholder="Password"/>
-  </div>
-  
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-    </div>
-  )
-}
+      <form onSubmit={handleSubmit}>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Name</label>
+          <input
+            type="name"
+            className="border-solid border-2 border-red-600	"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter name"
+          />
+        </div>
 
-export default Form
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="border-solid border-2 border-red-600	"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+          />
+        </div>
+
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="border-solid border-2 border-red-600	"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </div>
+
+        <button type="submit" className="bg-blue-500 ">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
