c161f363c3f89cf6a484933c8012a7697b7758fd
krishhackz <krishhackz.in@gmail.com>
2025-01-21 22:23:28 +0530

    using Link tag in header

--- Frontend/project/src/components/Header.jsx
+++ Frontend/project/src/components/Header.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Footer from './Footer'
-import Navbar from './Navbar'
-import Contact from './Contact'
 import { Link } from "react-router-dom"
 
 const Header = () => {
